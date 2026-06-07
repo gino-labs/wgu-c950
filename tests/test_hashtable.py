dbba2e0bd9d00700d974f1975ceb9b04e3faae85
Gino Curtis <ginocurtis5@gmail.com>
2026-06-07 12:04:14 -0700

    feat(tests): initial test code for hashtable

---
+++ tests/test_hashtable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from ..hashtable import HashTable
+
+test1_pkg_id = 99
+test1_pkg_weight = 10
+test1_address = "123 E Main St"
+test1_city = "Zootopia"
+test1_zip_code = 12345
+test1_deadline = "EOD"
+test1_status = "en route"
