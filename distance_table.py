bfe30168ca0dc54af6de22c4c90e6e0181f66282
Gino Curtis <ginocurtis5@gmail.com>
2026-06-26 14:17:01 -0700

    feat: move code to main.py for easier submission

--- distance_table.py
+++ distance_table.py
@@ -1,6 +1,7 @@
 import csv
 from pathlib import Path
 
+
 # Helper class for containing data for a neighbor
 class Neighbor:
     def __init__(self, name: str, address: str, distance: float):
@@ -8,13 +9,15 @@ class Neighbor:
         self.address = address
         self.distance = distance
 
+
 # Helper class for containing data for a hub
-class Hub:
+class DeliveryLocation:
     def __init__(self, name: str, address: str):
         self.name = name
         self.address = address
         self.address2 = None
         self.neighbors = []
+
 
 # Class for organizing distance table data
 class DistanceTable:
@@ -22,7 +25,7 @@ class DistanceTable:
         self.csv_file = Path(csv_file)
         if not self.csv_file.exists:
             raise FileNotFoundError(f"CSV file not found: {self.csv_file}")
-        self.hubs = []
+        self.locations = []
         self.build_table()
 
     # Parse data from table and return 2D list
@@ -50,12 +53,14 @@ class DistanceTable:
         for h, row in enumerate(parsed_table):
             if h == 0:
                 continue
+
             split_field = row[0].split("\n")
             hub_name = split_field[0]
             hub_address = split_field[1]
-            current_hub = Hub(hub_name, hub_address)
+
+            current_hub = DeliveryLocation(hub_name, hub_address)
             current_hub.address2 = row[1]
-            #print(f"Hub: {hub_name}\nHub Address: {hub_address}\n---")
+
             for i, column in enumerate(row):
                 if i < 2:
                     continue
@@ -63,14 +68,13 @@ class DistanceTable:
                     neighbor_split = top_row[i].split("\n")
                     neighbor_name = neighbor_split[0]
                     neighbor_address = neighbor_split[1]
+
                     if column:
                         neighbor_distance = column
                     else:
                         neighbor_distance = float(parsed_table[i - 1][h + 1])          
-                    #print(f"Neighbor: {neighbor_name}\nAddress: {neighbor_address}\nDistance: {neighbor_distance}mi\n---")
+
                     neighbor = Neighbor(neighbor_name, neighbor_address, neighbor_distance)
                     current_hub.neighbors.append(neighbor)
-            self.hubs.append(current_hub)
+            self.locations.append(current_hub)
                     
-distance_table = DistanceTable()
-print(len(distance_table.hubs))
