59c75a0d375aee8d4628f0a45c6995fc386e2ca3
Gino Curtis <ginocurtis5@gmail.com>
2026-06-26 17:15:37 -0700

    refactor: minimize project files

---
+++ data_structures.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+import csv
+from pathlib import Path
+
+# Helper class to represent a given package
+class Package:
+    def __init__(self, package_id, **package_data):
+        self.id = int(package_id)
+        self.weight = package_data.get("package_weight")
+        self.address = package_data.get("delivery_address")
+        self.city = package_data.get("delivery_city")
+        self.state = package_data.get("delivery_state")
+        self.zip_code = package_data.get("delivery_zip_code")
+        self.deadline = package_data.get("delivery_deadline")
+        self.status = package_data.get("delivery_status")
+        self.notes = package_data.get("special_notes")
+
+    # Add support for direct modulo operations
+    def __mod__(self, other):
+        return self.id % other
+
+
+### Hash Table data structure with tasks A & B
+class PackageHashTable:
+    def __init__(self, size=40):
+        self.packages = [None] * size
+
+    ### Requirement A
+    def add_package(self, package_id, package_weight=None, delivery_address=None, delivery_city=None, delivery_state=None, delivery_zip_code=None, delivery_deadline=None, delivery_status=None, special_notes=None):
+        package = Package(
+            package_id,
+            package_weight = package_weight,
+            delivery_address = delivery_address,
+            delivery_city = delivery_city, 
+            delivery_state = delivery_state, 
+            delivery_zip_code = delivery_zip_code,
+            delivery_deadline = delivery_deadline,
+            delivery_status = delivery_status,
+            special_notes = special_notes
+        )
+        # Modulo hash and assign Package to hashtable index
+        index = package % len(self.packages)
+        self.packages[index] = package
+
+    ### Requirement B
+    def get_package(self, package_id: int) -> Package:
+        # Return Package object at index
+        index = package_id % len(self.packages)
+        return self.packages[index]
+    
+    # Load package data from csv task files
+    def load_packages_from_csv(self, csv_file="task_files/wgups_package_file.csv"):
+        self.csv_file = Path(csv_file)
+        if not self.csv_file.exists:
+            raise FileNotFoundError(f"CSV file not found: {self.csv_file}")
+        
+        with open(csv_file, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+
+            start_loading = False
+            for row in reader:
+                if "Package" in row[0] and "ID" in row[0]:
+                    for i, header in enumerate(row):
+                        row[i] = header.replace("\n", " ").strip()
+
+                    package_id_index = row.index("Package ID")
+                    address_index = row.index("Address")
+                    city_index = row.index("City")
+                    state_index = row.index("State")
+                    zip_index = row.index("Zip")
+                    deadline_index = row.index("Delivery Deadline")
+                    weight_index = row.index("Weight KILO")
+                    notes_index = row.index("page 1 of 1PageSpecial Notes")
+                    start_loading = True
+                    continue
+                
+                if start_loading:
+                    print(f"Loading package {row[package_id_index]}")
+                    self.add_package(
+                        row[package_id_index],
+                        package_weight = row[weight_index],
+                        delivery_address = row[address_index],
+                        delivery_city = row[city_index],
+                        delivery_state = row[state_index],
+                        delivery_zip_code = row[zip_index],
+                        delivery_deadline = row[deadline_index],
+                        special_notes = row[notes_index]
+                    )
+
+
+# Helper class for containing data for a neighbor
+class Neighbor:
+    def __init__(self, name: str, address: str, distance: float):
+        self.name = name
+        self.address = address
+        self.distance = distance
+
+
+# Helper class for containing data for a delivery location
+class DeliveryLocation:
+    def __init__(self, name: str, address: str):
+        self.name = name
+        self.address = address
+        self.address2 = None
+        self.neighbors = []
+
+
+# Class for organizing distance table data
+class DistanceTable:
+    def __init__(self, csv_file="task_files/wgups_distance_table.csv"):
+        self.csv_file = Path(csv_file)
+        if not self.csv_file.exists:
+            raise FileNotFoundError(f"CSV file not found: {self.csv_file}")
+        self.locations = []
+        self.build_table()
+
+    # Parse data from table and return 2D list
+    def parse_distance_table(self):
+        with open(self.csv_file, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+
+            start_loading_data = False
+            parsed_table = []
+
+            for row in reader:
+                if "DISTANCE BETWEEN HUBS IN MILES" in row:
+                    start_loading_data = True
+
+                if start_loading_data:
+                    parsed_table.append(row)       
+        return parsed_table
+
+    # Restructure data into delivery locations with list of neighbors
+    def build_table(self):
+        # Table 28x29, index 1-27 == 2-28
+        parsed_table = self.parse_distance_table()
+
+        top_row = parsed_table[0]
+        for h, row in enumerate(parsed_table):
+            if h == 0:
+                continue
+
+            split_field = row[0].split("\n")
+            hub_name = split_field[0]
+            hub_address = split_field[1]
+
+            current_hub = DeliveryLocation(hub_name, hub_address)
+            current_hub.address2 = row[1]
+
+            for i, column in enumerate(row):
+                if i < 2:
+                    continue
+                else:
+                    neighbor_split = top_row[i].split("\n")
+                    neighbor_name = neighbor_split[0]
+                    neighbor_address = neighbor_split[1]
+                    
+                    if column:
+                        neighbor_distance = column
+                    else:
+                        neighbor_distance = float(parsed_table[i - 1][h + 1])          
+
+                    neighbor = Neighbor(neighbor_name, neighbor_address, neighbor_distance)
+                    current_hub.neighbors.append(neighbor)
+            self.locations.append(current_hub)
--- main.py
+++ main.py
@@ -1,170 +1,26 @@
-# Student ID: 011576592
-import csv
-from pathlib import Path
-from hashtable import HashTable
+"""
+Student ID: 011576592
+Student Name: Gino Curtis
+WGU C950 Data Structures and Algorithms 2
+
+Task Assumptions:
+•  Each truck can carry a maximum of 16 packages, and the ID number of each package is unique.
+•  The trucks travel at an average speed of 18 miles per hour and have an infinite amount of gas with no need to stop.
+•  There are no collisions.
+•  Three trucks and two drivers are available for deliveries. Each driver stays with the same truck as long as that truck is in service.
+•  Drivers leave the hub no earlier than 8:00 a.m., with the truck loaded, and can return to the hub for packages if needed.
+•  The delivery and loading times are instantaneous (i.e., no time passes while at a delivery or when moving packages to a truck at the hub). This time is factored into the calculation of the average speed of the trucks.
+•  There is up to one special note associated with a package.
+•  The delivery address for package #9, Third District Juvenile Court, is wrong and will be corrected at 10:20 a.m. WGUPS is aware that the address is incorrect and will be updated at 10:20 a.m. However, WGUPS does not know the correct address (410 S. State St., Salt Lake City, UT 84111) until 10:20 a.m.
+•  The distances provided in the “WGUPS Distance Table” are equal regardless of the direction traveled.
+•  The day ends when all 40 packages have been delivered.
+
+My Notes:
+- With assumption that each driver stays with the same truck, truck 3 is ignored due to only having 2 drivers available.
+
+"""
 
 
-# Helper class to represent a given package
-class Package:
-    def __init__(self, package_id, **package_data):
-        self.id = int(package_id)
-        self.weight = package_data.get("package_weight")
-        self.address = package_data.get("delivery_address")
-        self.city = package_data.get("delivery_city")
-        self.state = package_data.get("delivery_state")
-        self.zip_code = package_data.get("delivery_zip_code")
-        self.deadline = package_data.get("delivery_deadline")
-        self.status = package_data.get("delivery_status")
-        self.notes = package_data.get("special_notes")
-
-    # Add support for direct modulo operations A.K.A. hashing
-    def __mod__(self, other):
-        return self.id % other
-
-
-### Hash Table data structure with tasks A & B
-class PackageHashTable:
-    def __init__(self, size=40):
-        self.hashtable = [None] * size
-
-    ### Requirement A
-    def add_package(self, package_id, package_weight=None, delivery_address=None, delivery_city=None, delivery_state=None, delivery_zip_code=None, delivery_deadline=None, delivery_status=None, special_notes=None):
-        package = Package(
-            package_id,
-            package_weight = package_weight,
-            delivery_address = delivery_address,
-            delivery_city = delivery_city, 
-            delivery_state = delivery_state, 
-            delivery_zip_code = delivery_zip_code,
-            delivery_deadline = delivery_deadline,
-            delivery_status = delivery_status,
-            special_notes = special_notes
-        )
-        # Modulo hash and assign Package to hashtable index
-        index = package % len(self.hashtable)
-        self.hashtable[index] = package
-
-    ### Requirement B
-    def get_package(self, package_id: int) -> Package:
-        # Return Package object at index
-        index = package_id % len(self.hashtable)
-        return self.hashtable[index]
-    
-    # Load package data from csv task files
-    def load_packages_from_csv(self, csv_file="task_files/wgups_package_file.csv"):
-        self.csv_file = Path(csv_file)
-        if not self.csv_file.exists:
-            raise FileNotFoundError(f"CSV file not found: {self.csv_file}")
-        
-        with open(csv_file, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-
-            start_loading = False
-            for row in reader:
-                if "Package" in row[0] and "ID" in row[0]:
-                    for i, header in enumerate(row):
-                        row[i] = header.replace("\n", " ").strip()
-
-                    package_id_index = row.index("Package ID")
-                    address_index = row.index("Address")
-                    city_index = row.index("City")
-                    state_index = row.index("State")
-                    zip_index = row.index("Zip")
-                    deadline_index = row.index("Delivery Deadline")
-                    weight_index = row.index("Weight KILO")
-                    notes_index = row.index("page 1 of 1PageSpecial Notes")
-                    start_loading = True
-                    continue
-                
-                if start_loading:
-                    print(f"Loading package {row[package_id_index]}")
-                    self.add_package(
-                        row[package_id_index],
-                        package_weight = row[weight_index],
-                        delivery_address = row[address_index],
-                        delivery_city = row[city_index],
-                        delivery_state = row[state_index],
-                        delivery_zip_code = row[zip_index],
-                        delivery_deadline = row[deadline_index],
-                        special_notes = row[notes_index]
-                    )
-
-
-# Helper class for containing data for a neighbor
-class Neighbor:
-    def __init__(self, name: str, address: str, distance: float):
-        self.name = name
-        self.address = address
-        self.distance = distance
-
-
-# Helper class for containing data for a delivery location
-class DeliveryLocation:
-    def __init__(self, name: str, address: str):
-        self.name = name
-        self.address = address
-        self.address2 = None
-        self.neighbors = []
-
-
-# Class for organizing distance table data
-class DistanceTable:
-    def __init__(self, csv_file="task_files/wgups_distance_table.csv"):
-        self.csv_file = Path(csv_file)
-        if not self.csv_file.exists:
-            raise FileNotFoundError(f"CSV file not found: {self.csv_file}")
-        self.locations = []
-        self.build_table()
-
-    # Parse data from table and return 2D list
-    def parse_distance_table(self):
-        with open(self.csv_file, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-
-            start_loading_data = False
-            parsed_table = []
-
-            for row in reader:
-                if "DISTANCE BETWEEN HUBS IN MILES" in row:
-                    start_loading_data = True
-
-                if start_loading_data:
-                    parsed_table.append(row)       
-        return parsed_table
-
-    # Restructure data into delivery locations with list of neighbors
-    def build_table(self):
-        # Table 28x29, index 1-27 == 2-28
-        parsed_table = self.parse_distance_table()
-
-        top_row = parsed_table[0]
-        for h, row in enumerate(parsed_table):
-            if h == 0:
-                continue
-
-            split_field = row[0].split("\n")
-            hub_name = split_field[0]
-            hub_address = split_field[1]
-
-            current_hub = DeliveryLocation(hub_name, hub_address)
-            current_hub.address2 = row[1]
-
-            for i, column in enumerate(row):
-                if i < 2:
-                    continue
-                else:
-                    neighbor_split = top_row[i].split("\n")
-                    neighbor_name = neighbor_split[0]
-                    neighbor_address = neighbor_split[1]
-                    
-                    if column:
-                        neighbor_distance = column
-                    else:
-                        neighbor_distance = float(parsed_table[i - 1][h + 1])          
-
-                    neighbor = Neighbor(neighbor_name, neighbor_address, neighbor_distance)
-                    current_hub.neighbors.append(neighbor)
-            self.locations.append(current_hub)
 
 ### Main ###
 if __name__ == "__main__":
@@ -173,9 +29,6 @@ if __name__ == "__main__":
     distance_table = DistanceTable()
     package_hashtable = PackageHashTable()
     package_hashtable.load_packages_from_csv()
-    pkg7 = package_hashtable.get_package(7)
-    print(pkg7.address)
-
 
     # Phase 1: Load Trucks
 
