6ae86f86af818c214541dc1a40abd9a95decaabd
Gino Curtis <ginocurtis5@gmail.com>
2026-09-06 14:02:12 -0700

    refactor: consolidate ui to main.py

---
+++ algorithms.py
@@ -0,0 +1,11 @@
+from packages import Package
+from packages import PackageHashTable
+from locations import Location
+from locations import Neighbor
+from locations import DistanceTable
+
+def get_next_neighbor(current_location: Location):
+    pass
+
+def get_greedy_neighbor_list(package_hashtable: PackageHashTable):
+    pass
